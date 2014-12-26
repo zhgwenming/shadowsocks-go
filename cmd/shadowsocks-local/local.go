4465af6060982f76cfef8da1ded6e7b9cb611e82
Albert Zhang <zhgwenming@gmail.com>
2014-12-26 11:45:46 +0800

    just fallthrough if error happened

--- cmd/shadowsocks-local/local.go
+++ cmd/shadowsocks-local/local.go
@@ -443,6 +443,11 @@ func handleConnection(conn net.Conn) {
 			case received == 0:
 				// recoverable connection
 				remote.Close()
+				// just fallthrough is error happened
+				if err == nil {
+					conn.Close()
+					return
+				}
 				log.Printf("-- fallthrough to socks5 - %s (%s)", addr, err)
 			case received < 0:
 				log.Printf("[!!] add %s to remote cache - %d", addr, received)
